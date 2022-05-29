d53fbbd00a24cf037e06a96b9cb0dd52b5f7315f
Yipeng <gyp_516@163.com>
2022-05-29 13:28:03 +0800

    滚动题目

--- static/js/index.js
+++ static/js/index.js
@@ -2,13 +2,15 @@ let div = document.querySelector('.problem')
 let img = document.querySelector('#pi')
 
 let main = function () {
+    let n = 4;
     div.addEventListener('click', function (event) {
         console.log("Hello problem");
         console.log(event.type);
         div.innerHTML = 'Welcome to my website';
         console.log(document.getElementById('pi').src);
-        document.getElementById('pi').src = "static/images/questions/q4.png";
 
+        document.getElementById('pi').src = "static/images/questions/q" + n + ".png";
+        n += 1;
     });
 }
 
